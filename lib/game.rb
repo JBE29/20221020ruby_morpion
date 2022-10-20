d4d5936abcc1e327ea3fdc82ff41bf2b8a3c9ad7
JBE29 <jeanbaptiste.emily@gmail.com>
2022-10-20 09:40:26 +0200

    2nd push

---
+++ lib/game.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Game
+    attr_accessor :player
+    A1 = " "
+    A2 = " "
+    A3 = " "
+    B1 = " "
+    B2 = " "
+    B3 = " "
+    C1 = " "
+    C2 = " "
+    C3 = " "
+    puts "#{A1} | #{B1} | #{C1}"
+    puts "#{A2} | #{B2} | #{C2}"
+    puts "#{A3} | #{B3} | #{C3}"
+end
